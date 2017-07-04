44bb5b204f7b2c5d395a504434ee6336bed20562
VanLock11 <van11khachatryan@gmail.com>
2017-07-04 16:07:28 +0400

    My first experience with threads

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void WriteSecond()
+        {
+            Random rd = new Random();
+            while (true)
+            {
+                Console.ForegroundColor = (ConsoleColor)rd.Next(1, 16);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart writesecond = new ThreadStart(WriteSecond);
+            Thread thredinger = new Thread(writesecond);
+            thredinger.Start();
+            Console.WriteLine("Enter your Text");
+            string s = Console.ReadLine();
+            thredinger.Abort();
+            Console.ResetColor();
+        }
+    }
+}
